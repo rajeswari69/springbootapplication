9056140c13991fd9d1e6a4ad50827bced64fcf18
rajeswari69 <rajeswarigottipalli69>
2023-04-02 09:45:46 +0530

    added springoot application

---
+++ springboot-demo/springboot-demo/src/test/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.star.agile.assignment.springbootdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
